c67a5e9d31799a76b3a27bb70ef34132478a0baa
向阳 <xiangqian_liu@163.com>
2022-06-20 11:08:26 +0800

    feat: 添加一些用于提示的参数

--- src/cli.ts
+++ src/cli.ts
@@ -2,15 +2,25 @@
 
 import argv from 'minimist'
 import ansiColors from 'ansi-colors'
+import { Command } from 'commander'
 import compression from './core'
 import { ICompressParams } from './types'
 import { formatCompressParams, getVersion } from './utils'
 
+const program = new Command()
 const argvObj = argv(process.argv.slice(2))
 
-if (argvObj.v || argvObj.V || argvObj.version) {
-  const version = getVersion()
-  console.log(ansiColors.yellow(version))
+if (argvObj.help || argvObj.h) {
+  program.name('Fzip')
+    .description('A compression tool')
+    .version(getVersion())
+    .option('-f, --from', 'path to compressed file')
+    .option('-o, --output', 'compression result output path')
+    .option('-l, --level', 'compression level, you can use 1~9')
+    .option('-n, --name', 'rename compression result name')
+  program.parse()
+} else if (argvObj.V || argvObj.version) {
+  console.log(ansiColors.yellow(getVersion()))
 } else {
   const compressParams: ICompressParams = formatCompressParams(argvObj)
   compression(compressParams)
